77c638211e3b6308a9faa0e7a050937ea9661bf8
Eliseu Videira <eliseuvideira1@gmail.com>
2020-04-25 22:22:05 -0300

    added eslint, prettier and husky for consistent code style

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,36 @@
-const { join } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const DotEnvPlugin = require("dotenv-webpack");
-const TerserPlugin = require("terser-webpack-plugin");
+/* eslint-disable */
+const { join } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DotEnvPlugin = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const config = {
-  entry: ["react-hot-loader/patch", join(__dirname, "src", "index.tsx")],
+  entry: ['react-hot-loader/patch', join(__dirname, 'src', 'index.tsx')],
   output: {
-    path: join(__dirname, "build"),
-    filename: "bundle.js",
+    path: join(__dirname, 'build'),
+    filename: 'bundle.js',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ["ts-loader"],
+        use: ['ts-loader'],
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: join(__dirname, "src", "index.html"),
-      favicon: join(__dirname, "src", "favicon.ico"),
+      template: join(__dirname, 'src', 'index.html'),
+      favicon: join(__dirname, 'src', 'favicon.ico'),
     }),
     new DotEnvPlugin({
       safe: true,
     }),
   ],
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   optimization: {
     minimizer: [
