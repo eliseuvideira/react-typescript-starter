a29b9805c26cbd61e5d80298165ee27807f4d988
Eliseu Videira <eliseuvideira1@gmail.com>
2020-07-31 23:10:54 -0300

    Added babel for hot-reloading in dev-mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/layout';
 
-const Index = () => (
-  <Layout title="Home">
-    <div>Home</div>
-  </Layout>
-);
+const Index = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Layout title="Home">
+      <div>Home</div>
+      <div>{count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+    </Layout>
+  );
+};
 
 export default Index;
--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,103 @@
 const webpack = require('webpack');
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 const { required: requiredKeys } = require('dotenv-safe').config();
 
-/** @type {webpack.Configuration} */
-const config = {
-  entry: ['react-hot-loader/patch', join(__dirname, 'src', 'index.tsx')],
-  output: {
-    path: join(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  devtool:
-    process.env.NODE_ENV === 'development' ? 'inline-source-map' : 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['react-hot-loader/webpack', 'ts-loader'],
-        exclude: /node_modules/,
+/**
+ * @returns {webpack.Configuration}
+ */
+const getConfig = () => {
+  if (isDev) {
+    return {
+      entry: join(__dirname, 'src', 'index.tsx'),
+      output: {
+        path: join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/',
       },
+      devServer: {
+        historyApiFallback: true,
+      },
+      devtool: 'eval-source-map',
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                cacheDirectory: true,
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    { targets: { browsers: 'last 2 versions' } },
+                  ],
+                  '@babel/preset-typescript',
+                  '@babel/preset-react',
+                ],
+                plugins: ['react-hot-loader/babel'],
+              },
+            },
+          },
+        ],
+      },
+      performance: false,
+      plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.EnvironmentPlugin(requiredKeys),
+        new HtmlWebpackPlugin({
+          template: join(__dirname, 'public', 'index.html'),
+          favicon: join(__dirname, 'public', 'favicon.ico'),
+        }),
+      ],
+      resolve: {
+        modules: ['node_modules'],
+        alias: {
+          'react-dom': '@hot-loader/react-dom',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    };
+  }
+
+  return {
+    entry: join(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: join(__dirname, 'build'),
+      filename: 'bundle.js',
+      publicPath: '/',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: ['ts-loader'],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    performance: false,
+    plugins: [
+      new webpack.EnvironmentPlugin(requiredKeys),
+      new HtmlWebpackPlugin({
+        template: join(__dirname, 'public', 'index.html'),
+        favicon: join(__dirname, 'public', 'favicon.ico'),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: join(__dirname, 'public', 'index.html'),
-      favicon: join(__dirname, 'public', 'favicon.ico'),
-    }),
-    new webpack.EnvironmentPlugin(requiredKeys),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  performance: false,
-  devServer: {
-    historyApiFallback: true,
-  },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+  };
 };
+
+const config = getConfig();
 
 module.exports = config;
